e4c4f00c4cab7f4198fdd2ff59c2af7280fca0c8
valdadyne <rodgers.gitau@gmail.com>
2017-07-28 10:42:31 +0300

    Dashboard page

---
+++ src/components/App.jsx
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import {Navbar, Nav, NavItem, Grid, Row, Col, ButtonToolbar, ButtonGroup, Button} from 'react-bootstrap';
+
+import AddTask from './AddTask';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Navbar default collapseOnSelect>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <a href="/">WHOLELIFE</a>
+            </Navbar.Brand>
+          </Navbar.Header>
+          <Navbar.Collapse>
+            <Nav pullRight>
+              <NavItem eventKey={1} href="/">SignOut</NavItem>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+        <Grid>
+            <Row className="show-grid">
+              <Col xs={12} md={8}>
+                <ButtonToolbar>
+                  <ButtonGroup>
+                    <Button>Open Tasks</Button>
+                    <Button>Completed Tasks</Button>
+                    <Button>Past Due Tasks</Button>
+                  </ButtonGroup>
+                </ButtonToolbar>
+              </Col>
+              <Col xs={6} md={4}>
+                <AddTask />
+              </Col>
+            </Row>          
+        </Grid>
+       </div>
+    );
+  }
+}
+
+export default App;
