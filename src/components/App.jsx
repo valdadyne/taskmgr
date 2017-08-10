2c9b1c2768dbfdc54119c1517344c4f313d15550
valdadyne <rodgers.gitau@gmail.com>
2017-08-10 15:56:14 +0300

    Changed UI

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,9 +2,6 @@ import React, { Component } from 'react';
 import { connect} from 'react-redux';
 import { firebaseApp} from '../firebase';
 
-import {Navbar, Nav, NavItem, Grid, Row, Col} from 'react-bootstrap';
-import { ButtonToolbar , ButtonGroup, Button} from 'react-bootstrap';
-
 import AddTask from './AddTask';
 import TaskList from './TaskList';
 import CompletedTaskList from './CompletedTaskList';
@@ -15,64 +12,19 @@ class App extends Component {
   }
   render() {
     return (
-      <div className="App">
-        <Navbar default collapseOnSelect>
-          <Navbar.Header>
-            <Navbar.Brand>
-              <a href="/">WHOLELIFE</a>
-            </Navbar.Brand>
-          </Navbar.Header>
-          <Navbar.Collapse>
-            <Nav pullRight>
-              <NavItem className="btn-link" onClick={() =>this.signOut()}>SignOut</NavItem>
-            </Nav>
-          </Navbar.Collapse>
-        </Navbar>
-        <Grid>
-            <Row className="show-grid">
-              <Col xs={12} md={8}>
-              <ButtonToolbar>
-                <ButtonGroup>
-                  <Button className="button">
-                    <a data-toggle="tab" href="#openTasks" className="text-primary">Open Tasks</a>
-                  </Button>
-                  <Button className="button">
-                    <a data-toggle="tab" href="#completedTasks" className="text-success">Completed Tasks</a>
-                  </Button>
-                  <Button className="button">
-                    <a data-toggle="tab" href="#pastDueTasks" className="text-danger">Past Due Tasks</a>
-                  </Button>
-                </ButtonGroup>                                                                                                                                                                
-              </ButtonToolbar>
-              <div className="content-wrapper">
-                <div className="tab-content">
-                  <div id="openTasks" className="tab-pane fade in active">
-                    <h4 className="subtitle">Open Tasks</h4>
-                    <TaskList />
-                  </div>
-                  <div id="completedTasks" className="tab-pane fade">
-                    <h4 className="subtitle">Completed Tasks</h4>
-                    <CompletedTaskList />
-                  </div>
-                  <div id="PastDueTasks" className="tab-pane fade">
-                    <h4 className="subtitle">Past Due Tasks</h4>
-                    <p>Some content in menu 2.</p>
-                  </div>
-                </div>
-              </div>                
-                
-              </Col>
-              <Col xs={6} md={4}>
-                <AddTask />
-              </Col>
-            </Row>          
-        </Grid>
+      <div className="app-wrapper">
+        <nav className="navbar">
+          <ul className="nav navbar-nav navbar-right">
+            <li><a href="/"><i className="material-icons">supervisor_account</i>Admin</a></li>
+            <li><a href="/"><i className="material-icons">person_outline</i>SignOut</a></li>
+          </ul>
+          <a className="navbar-brand" href="/">WholeLife</a>
+        </nav>
        </div>
     );
   }
 }
 function mapStateToProps(state) {
-    // console.log('state', state);
     return{}
 }
 
