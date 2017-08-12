0159adc84bcaf5d2f918554e8b08536f414949c4
valdadyne <rodgers.gitau@gmail.com>
2017-08-12 13:45:37 +0300

    Form Validation before Adding Tasks

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -16,23 +16,20 @@ class AddTask extends Component{
             priority:''
         }
     }
-    addTask(){
-        let msg = "";
+    addTask(e){
         const taskname = this.state.taskname;
         const assigned = this.state.assigned;
         const assigned_reliever = this.state.assigned_reliever;
         const start_date = this.state.due_date;
         const due_date = this.state.due_date;
-        const priority = this.state.priority;
-        if (taskname)        
-            taskRef.push({taskname,assigned,assigned_reliever,start_date,due_date,priority});
-        msg = "You need atleast a TaskName";
-        this.setState({taskname: msg});
+        const priority = this.state.priority;        
+        taskRef.push({taskname,assigned,assigned_reliever,start_date,due_date,priority});
+        e.stopPropagation()     
     }
     render(){
         return(
             <div id="AddTask" className="modal fade">
-                <div className="modal-content">
+                <form className="modal-content" onSubmit={(e) => this.addTask()}>
                     <div className="modal-header">
                         <button type="button" className="close" data-dismiss="modal">&times;</button>
                         <h4 className="modal-title">Add New Task</h4>
@@ -42,46 +39,46 @@ class AddTask extends Component{
                         <div className="col-sm-6">
                             <div className="form-group">
                                 <label htmlFor="taskname">Taskname</label>
-                                <input className="form-control" type="text" id="taskname"
+                                <input className="form-control" type="text" id="taskname" required
                                     onChange={event => this.setState({taskname:event.target.value})} />
                             </div>
                             <div className="form-group">
                                 <label htmlFor="assigned">Assigned</label>
-                                <input className="form-control" type="text" id="assigned"
+                                <input className="form-control" type="text" id="assigned" required
                                     onChange={event => this.setState({assigned:event.target.value})} />
                             </div>
                             <div className="form-group">
                                 <label htmlFor="reliever">Assisted By</label>
-                                <input className="form-control" type="text" id="reliever"
+                                <input className="form-control" type="text" id="reliever" required
                                     onChange={event => this.setState({assigned_reliever:event.target.value})}/>
                             </div>
                         </div>
                         <div className="col-sm-6">
                             <div className="form-group">
                                 <label htmlFor="start_date">Start Date</label>
-                                <input className="form-control" type="date" id="start_date"
+                                <input className="form-control" type="date" id="start_date"  max= "#due_date" required
                                     onChange={event => this.setState({start_date:event.target.value})} />
                             </div>
                             <div className="form-group">
                                 <label htmlFor="due_date">Due Date</label>
-                                <input className="form-control" type="date" id="due_date"
+                                <input className="form-control" type="date" id="due_date" min="#start_date" required
                                     onChange={event => this.setState({due_date:event.target.value})} />
                             </div>
                             <div className="form-group priority">
                                 <label>Priority</label>
                                 <div>
                                     <label className="form-control-static">
-                                        <input type="radio" value ="High" 
+                                        <input type="radio" value ="High" required
                                             onChange={event => this.setState({priority:event.target.value})}/>
                                             &nbsp;High
                                     </label>
                                     <label className="form-control-static">
-                                        <input type="radio" value ="Medium"
+                                        <input type="radio" value ="Medium" required
                                             onChange={event => this.setState({priority:event.target.value})}/>
                                             &nbsp;Medium
                                     </label>
                                     <label className="form-control-static">
-                                        <input type="radio" value ="Low" 
+                                        <input type="radio" value ="Low" required
                                             onChange={event => this.setState({priority:event.target.value})}/>
                                             &nbsp;Low
                                     </label>
@@ -91,14 +88,12 @@ class AddTask extends Component{
                     </div>
                     </div>
                     <div className="modal-footer">
-                        <button type="button" className="pull-left btn btn-success"
-                            onClick={() => this.addTask()}> AddTask
-                        </button>
+                        <button type="submit" className="pull-left btn btn-success"> AddTask </button>
                         <button  type="button" className="pull-right btn btn-default" data-dismiss="modal">
                             Cancel
                         </button>
                     </div>
-                </div>
+                </form>
             </div>        
         );
     }
