3d06eee8c559e2f6302e1eb5aaeb7008a99b6661
valdadyne <rodgers.gitau@gmail.com>
2017-08-22 16:47:58 +0300

    Layout changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
+
+import TaskList from './components/Task/TaskList';
 import logo from './logo.png';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-     
+      <div className="app-wrapper">
+        <header>
+          <h2 className="brand">WholeLife </h2>
+        </header>
+        <main>
+          <TaskList />
+        </main>
+        <footer></footer>     
       </div>
     );
   }
