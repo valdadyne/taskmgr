8452e86003e621164e908e36e399832a0a1518cd
valdadyne <rodgers.gitau@gmail.com>
2017-08-24 17:00:19 +0300

    Added Firebase & Connect App.js to redux

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
 import React, { Component } from 'react';
+import {firebaseApp} from './firebase';
+import { connect } from 'react-redux';
 
-import TaskList from './components/Task/TaskList';
-import logo from './logo.png';
+import {AddTask, TaskList} from './components/Task';
 import './App.css';
 
 class App extends Component {
+  signOut(){
+    firebaseApp.auth().signOut();
+  }
   render() {
     return (
       <div className="app-wrapper">
         <header>
           <h2 className="brand">WholeLife </h2>
+          <div className="button-group">
+            <button className="btn btn-danger"
+              onClick={() => this.signOut()}>SignOut</button>
+          </div>
         </header>
         <main>
           <TaskList />
         </main>
-        <footer></footer>     
+        <footer></footer>
       </div>
     );
   }
 }
 
-export default App;
+function mapStateToProps(state){
+  // console.log('state', state);
+  return {}
+}
+export default connect(mapStateToProps, null)(App);
