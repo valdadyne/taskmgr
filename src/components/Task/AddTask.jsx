f8680e74003ce36a5c263fe2b0258fe43fbfbbb3
valdadyne <rodgers.gitau@gmail.com>
2017-08-25 16:46:25 +0300

    Styling for AddTask form

--- src/components/Task/AddTask.jsx
+++ src/components/Task/AddTask.jsx
@@ -21,15 +21,17 @@ class AddTask extends Component{
   }
   render(){
     return (
-      <form className="form" onSubmit={(e) => {this.addTask();e.preventDefault();}}>
-        <div className="form-group">
-          <div className="row">
-            <div className="col-md-6">
+      <form className="form AddTask" onSubmit={(e) => {this.addTask();e.preventDefault();}}>
+        <div className="row">
+          <div className="col-sm-6">
+            <div className="form-group">
               <label>Taskname</label>
               <input type="text" className="form-control"  value= {this.state.taskname} placeholder="Enter taskname"
-                onChange={event => this.setState({taskname:event.target.value})}/>
+                onChange={event => this.setState({taskname:event.target.value})} required />
             </div>
-            <div className="col-md-6">
+          </div>
+          <div className="col-sm-6">
+            <div className="form-group">
               <label>Priority</label>
               <select className="form-control" value= {this.state.priority}
                   onChange={event => this.setState({priority:event.target.value})}>
@@ -43,24 +45,23 @@ class AddTask extends Component{
         <div className="form-group">
           <label>Task Description</label>
           <textarea rows="5" className="form-control" value= {this.state.description} placeholder="Enter the description of the task"
-            onChange={event => this.setState({description:event.target.value})} >
+            onChange={event => this.setState({description:event.target.value})} required>
           </textarea>
         </div>
-        <div className="form-group">
-          <div className="row">
-            <div className="col-md-6">
+        <div className="row">
+          <div className="col-sm-6">
+            <div className="form-group">
               <label>Start Date</label>
               <input type="date" className="form-control" value= {this.state.start_date}
-                onChange={event => this.setState({start_date:event.target.value})}/>
-            </div>
-            <div className="col-md-6">
-              <label>Due Date</label>
-              <input type="date" className="form-control" value= {this.state.due_date}
-                onChange={event => this.setState({due_date:event.target.value})}/>
+                onChange={event => this.setState({start_date:event.target.value})} required/>
             </div>
           </div>
+          <div className="col-sm-6">
+            <label>Due Date</label>
+            <input type="date" className="form-control" value= {this.state.due_date}
+              onChange={event => this.setState({due_date:event.target.value})} required/>
+          </div>
         </div>
-        <br />
         <div className="form-group">
           <button type="submit" className="btn btn-success"> Add Task</button>
         </div>
