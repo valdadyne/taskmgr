410237bfc2c82ff5860c4fa6167c5ae8c24ed26a
valdadyne <rodgers.gitau@gmail.com>
2017-08-27 16:25:57 +0300

    Rendering Completed Tasks

--- src/components/Task/CompleteTasks.jsx
+++ src/components/Task/CompleteTasks.jsx
@@ -2,6 +2,7 @@ import React, { Component} from 'react';
 import { connect} from 'react-redux';
 import { setCompleted} from '../../actions';
 import { completeTaskRef } from '../../firebase';
+import moment from 'moment';
 
 class CompleteTaskList extends Component {
   componentDidMount(){
@@ -15,15 +16,60 @@ class CompleteTaskList extends Component {
     })
   }
   render(){
-    console.log('this.props.completeTasks', this.props.completeTasks);
-    const {closer, task} = this.props.completeTasks;
     return (
-      <div className="CompleteTasks">
+      <div className="completedList">
+        {
+          this.props.completeTasks.map(completeTask =>{
+            return(
+              <Task key={completeTask.task.id} completeTask={completeTask}/>)
+            })
+        }
 
       </div>
     )
   }
+}
 
+const Task = (props) => {
+  const {closer} = props.completeTask;
+  const {taskname,description, priority,start_date,due_date, Creator} = props.completeTask.task;
+  return(
+    <div className="completedItem">
+      <div className="panel panel-default">
+        <div className="panel-heading">
+          <h3 className="panel-title">
+            <strong>{taskname}</strong>
+          </h3>
+          <h3 className="panel-title">
+            <strong>{moment(due_date).diff(moment(start_date), 'days')} days</strong>
+          </h3>
+          <span className="pull-right clickable">
+            <i className="glyphicon glyphicon-chevron-up"></i>
+          </span>
+        </div>
+        <div className="panel-body">
+          <table>
+            <tr>
+              <td>Description</td>
+              <td className="long">{description}</td>
+            </tr>
+            <tr>
+              <td>Priority</td>
+              <td>{priority}</td>
+            </tr>
+            <tr>
+              <td>Start Date</td>
+              <td>{start_date}</td>
+            </tr>
+            <tr>
+              <td>Due Date</td>
+              <td>{due_date}</td>
+            </tr>
+          </table>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 function mapStateToProps(state){
